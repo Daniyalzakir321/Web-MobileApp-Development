0455279664a1020d6c8c95f3cd835673771f1621
Daniyal Zakir <daniyalzakir03@gmail.com>
2020-05-29 01:23:38 +0500

    Assignment -4 Mobile Price.PK Responsive Website

---
+++ Assignment -4/app.js
@@ -0,0 +1,31 @@
+
+function BuyNow()
+{
+
+Swal.fire({
+title: 'Coming Soon',
+imageUrl: 'marqee-1/comingsoon.gif',
+imageWidth: 320,
+imageHeight: 210,
+imageAlt: 'Buy Now Option, Will Be Available Soon.',})
+
+}
+
+
+
+function Explore()
+{
+
+Swal.fire({
+text: 'SPECIFICATIONS',
+imageUrl: 'marqee-1/specifications.png',
+imageWidth: 420,
+imageHeight: 420,
+imageAlt: 'Specifications'})
+
+
+}
+
+
+
+
