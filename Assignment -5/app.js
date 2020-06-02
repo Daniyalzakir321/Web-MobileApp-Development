f5eb1704f1c3ec81f46fa21e7ade0925d4ca8c81
Daniyal Zakir <daniyalzakir03@gmail.com>
2020-06-02 17:00:31 +0500

    Assignment-5 JavaScript 20-Chapters Practice

---
+++ Assignment -5/app.js
@@ -0,0 +1,192 @@
+// CHAPTER NO: 1
+// Task  : 1.
+// alert('Hello!');
+
+// Task  : 2.
+// alert('Error! Please enter a valid password.');
+
+// Task  : 3.
+// alert('Welcome to JS Land...\n'+'Happy Coding!');
+
+// Task  : 4.
+// alert('Welcome to JS Land...');
+// alert('Happy Coding!');
+
+// Task  : 5.
+// console.log("Hello... I can run JS through my web browser's console");
+
+// Task  : 6.
+// alert(' Use of alerts in my new/existing HTML & CSS project. Done...');
+
+// Task  : 7.
+// a. Head  [On Html Page]
+// b. Body (before your page’s HTML) [On Html Page]
+// c. Body (inside your page’s HTML) [On Html Page]
+// d. Body (after your page’s HTML)  [On Html Page]
+
+
+
+
+
+
+
+// CHAPTER NO: 2
+// Task  : 1.
+// var username
+
+// Task  : 2.
+// var myName= "Muhammad Daniyal Zakir"
+
+// Task  : 3.
+// var message
+// message="Hello World"
+// alert(message);
+
+// Task  : 4.
+// var name ="Jhone Doe"
+// var age ="15 years old"
+// var profession ="Certified Mobile Application Development"
+// alert(name);
+// alert(age);
+// alert(profession);
+
+// Task  : 5.
+// alert('PIZZA\n'+'PIZZ\n'+'PIZ\n'+'PI\n'+'P');
+
+// Task  : 6.
+// var email ="example@example.com"
+// alert('My email address is '+ email);
+
+// Task  : 7.
+// var book ="A smarter way to learn JavaScript"
+// alert("I am trying to learn from the book " + book);
+
+// Task  : 8.
+// document.write('Yah! I can write HTML, content through javaScript');
+
+// Task  : 9.
+// var decoration="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(decoration);
+
+
+
+
+
+
+
+
+// CHAPTER NO: 3
+// Task  : 1.
+// var myage="22"
+// alert("I am " +myage+ " years old");
+
+// Task  : 2.
+// var site_visits= "14"
+// alert("You have visited this site "+ site_visits +" times");
+
+// Task  : 3.
+// var birthYear="1997"
+// document.write(" My birth year is " + birthYear.italics().bold() + "<br> Data type of my decleared variable is number");
+
+
+// Task  : 4.
+// var visitor_name = "John Doe"
+// var product_title ="T-shirt"
+// var quantity = "5"
+// document.write( visitor_name.bold() + " ordered " + quantity.bold() + " " +product_title.bold() +"(s) on XYZ Clothing Store");
+
+
+
+
+
+
+
+
+// CHAPTER NO: 4
+// Task  : 1.
+// var var1,var2,var3
+
+// // Task  : 2.
+// var Normal, $dollar, numberinend22, camelCase, _underscore 
+// var  var, 22numberinstart, @specialcharacter, .dot, ?question
+
+// Task  : 3. 
+// document.write(" <h1> Rules for naming JS variables </h1>")
+// document.write("Variable names can only contain <b>number</b>, <b>$</b>, and <b>_</b> . For example $my_1stVariable.   <br>")
+// document.write("Variables must begin with a <b>letter</b>, <b>$</b>, or <b>_</b>. For example $name, _name or name.   <br>")
+// document.write("Variable names are case <b>sensitive</b>.  <br>")
+// document.write("Variable names should not be JS  <b>keywords</b>.  <br>")
+
+
+
+
+// CHAPTER NO: 5
+// Task  : 1.
+
+
+// Task  : 2.
+
+
+
+// Task  : 3.
+
+
+
+// Task  : 4.
+
+
+
+// Task  : 5.
+
+
+
+
+
+
+
+// CHAPTER NO: 6
+// Task  : 1.
+
+
+// Task  : 2.
+
+
+
+// Task  : 3.
+
+
+
+// Task  : 4.
+
+
+
+// Task  : 5.
+
+
+
+
+
+
+
+// CHAPTER NO: 7
+// Task  : 1.
+
+
+// Task  : 2.
+
+
+
+// Task  : 3.
+
+
+
+// Task  : 4.
+
+
+
+// Task  : 5.
+
+
+
+
+
