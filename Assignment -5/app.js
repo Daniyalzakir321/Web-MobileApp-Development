72115ed0d1c301fe11ccd2db11aebcc30e51459f
Daniyal Zakir <daniyalzakir03@gmail.com>
2020-06-03 18:22:01 +0500

    Modified

--- Assignment -5/app.js
+++ Assignment -5/app.js
@@ -122,69 +122,221 @@
 
 // CHAPTER NO: 5
 // Task  : 1.
-
+// var a=3,b=5
+// c=a+b
+// document.write("Sum of "+a+" and "+b+" is "+c)
 
 // Task  : 2.
+// var a=3,b=5
+// c=a-b
+// document.write("Subtraction of "+a+" and "+b+" is "+c+"<br>")
+
+// c=a*b
+// document.write("Multiplicationn of "+a+" and "+b+" is "+c+"<br>")
+
+// c=a/b
+// document.write("Division of "+a+" and "+b+" is "+c+"<br>")
+
+// c=a%b
+// document.write("Modulus of "+a+" and "+b+" is "+c+"<br>")
 
 
 
 // Task  : 3.
+// var a
+// document.write("Value after variable declaration is: "+a+ "<br>")
+
+// a=5
+// document.write("Initial value: "+a+"<br>")
+
+// a++
+// document.write("Value after increment is: "+a+"<br>")
+
+// a=a+7
+// document.write("Value after addition is: "+a+"<br>")
+
+// a--
+// document.write("Value after decreament is: "+a+"<br>")
+
+// a=a%3
+// document.write("The remainder is: "+a+"<br>")
 
 
 
 // Task  : 4.
+// var movie=600, buy=5
+// document.write("The cost of buy "+buy+" tickets to a movie is "+movie+"PKR")
 
 
 
 // Task  : 5.
+// var table=4
+// document.write("Table of "+ table+ "<br>")
+// for( var i=1; i<=10; i++){
+
+// document.write( table + " * " + i + " = "+ i*table + "<br>")
+// }
+
+
+
+// Task  : 6.
+
+// var celsius=25
+// fahrenheit=(celsius*9/5)+32
+// document.write(celsius+"<sup>0</sup>C is "+ fahrenheit + "<sup>o</sup>F<br>")
+
+// var fahrenheit=70
+// celsius=(fahrenheit-32)*5/9
+// document.write(fahrenheit + "<sup>0</sup>F is " +celsius+"<sup>o</sup>C <br>" )
+
+
+// Task  : 7.
+// var price_of_item_1=650, price_of_item_2=100, quantity_of_item_1=3, quantity_of_item_2=7, shipping_charges=100
+// total_cost=(price_of_item_1*quantity_of_item_1) + (price_of_item_2*quantity_of_item_2)+shipping_charges
+
+// document.write("Price of item 1 is " +price_of_item_1+  "<br>")
+// document.write("Quantity of item 1 is " +quantity_of_item_1+  "<br>")
+
+// document.write("Price of item 2 is " +price_of_item_2+  "<br>")
+// document.write("Quantity of item 2 is " +quantity_of_item_2+  "<br>")
+
+// document.write("Shipping Charges " +shipping_charges+  "<br> <br>")
+// document.write("Total cost of your order is  " +total_cost+  "<br>")
 
 
 
 
+// Task  : 8.
+// document.write("<h1> Marks Sheet</h1>")
+
+// var total_marks=980, marks_obtained=804
+// percentage=(marks_obtained*100)/total_marks
+
+// document.write("total marks: "+total_marks+"<br>")
+// document.write("Marks obtained: "+marks_obtained+"<br>")
+// document.write("Percentage: "+percentage+"%")
 
 
 
-// CHAPTER NO: 6
+// Task  : 9.
+// document.write("<h1>Currency in PKR</h1>")
+
+// var dol=prompt("Enter Currency in Dollar $ ")
+// var riy=prompt("Enter Currency in Riyal ﷼ ")
+// dollar= dol*104.80
+// riyal= riy*28
+
+// document.write(` <h3> ${dol} Dollar  Convert to PKR</h3>`)
+// document.write("Total Currency in PKR: "+dollar+"<br>")
+
+// document.write( ` <h3> ${riy} Riyal  Convert to PKR</h3>`)
+// document.write("Total Currency in PKR: "+riyal+"<br>")
+
+
+
+
+// Task  : 10
+// var x=1, n=x+5, y=x*10, z=x/2 
+// document.write("Original Value = " +x+ "<br>After Add " +n+ "<br>After Mul " +y+ "<br>After Div "+z+ "<br>")
+
+
+
+// Task  : 11
+// document.write("<h1>Age Calculator</h1>")
+// var age=prompt("Enter Current Year"), birth=prompt("Enter Your Date of Birth")
+// modify= age-birth
+// document.write("Current Year: "+age+" <br> Birth Year: "+birth+"<br>Your Age is: "+modify)
+
+
+// Task  : 12
+// document.write("<h1>The Geometrizer</h1>")
+// var π = 3.142, r=prompt("Enter Radius Of Circle ")
+// circumference= 2 *π *r
+// area= π*(r**2)
+
+// document.write("Radius of circle is: "+r+"<br>")
+// document.write("The circumference is: "+circumference+"<br>")
+// document.write("The area is: "+area+"<br>")
+
+
+
+// Task  : 13
+// document.write("<h1>The Lifetime Supply Calculator</h1>")
+// var snack="Chocklate Chip", age=15, max_age=65, estimated_amount=3
+// document.write("Favorite Snack : "+snack+"<br>")
+// document.write("Current age : "+age+"<br>")
+// document.write("Estimated Maximum Age : "+max_age+"<br>")
+// document.write("Amount of snacks per day: "+estimated_amount+"<br>")
+// document.write(`You will need ${estimated_amount*age} ${snack} to last you until the ripe old age of ${max_age} `)
+
+
+
+
+// CHAPTER NO:6
 // Task  : 1.
+// document.write( "Result:")
+// var a=10
+// document.write( "The value of a is: "+a)
+// document.write( "<br>,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,<br><br><br>")
+
+// ++a
+// document.write( "The value of ++a is: "+a+"<br>")
+// document.write( "Now the value of a is: "+a+"<br><br><br>")
+
+// a++
+// document.write( "The value of a++ is: "+a+"<br>")
+// document.write( "Now the value of a is: "+a+"<br><br><br>")
+
+// --a
+// document.write( "The value of --a is: "+a+"<br>")
+// document.write( "Now the value of a is: "+a+"<br><br><br>")
+
+// a--
+// document.write( "The value of a-- is: "+a+"<br>")
+// document.write( "Now the value of a is: "+a+"<br><br><br>")
+
 
 
 // Task  : 2.
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// --a;
+// --a - --b;
+// --a - --b + ++b;
+// --a - --b + ++b + b--;
 
 
 
 // Task  : 3.
-
-
-
-// Task  : 4.
-
-
-
-// Task  : 5.
-
-
-
-
-
-
-
-// CHAPTER NO: 7
-// Task  : 1.
-
-
-// Task  : 2.
-
-
-
-// Task  : 3.
-
+// var user_input=prompt("Enter Your Name")
+// document.write("Hello! "+user_input)
 
 
 // Task  : 4.
-
+// var num=prompt("Enter Number")
+// for( var i=1; i<=10; i++){
+// document.write( num + " * " + i + " = "+ i*num + "<br>")
+// }
 
 
 // Task  : 5.
+// var total=100, english=prompt("Enter English Obtained Marks"),math=prompt("Enter Math Obtained Marks"),urdu=prompt("Enter Urdu Obtained Marks")
+// per=(english*100)/total
+// per1=(math*100)/total
+// per2=(urdu*100)/total
+
+// obt=english+urdu+math
+// pertotal=(obt*100)/300
+
+// document.write("Subject   &nbsp; Total_Marks   &nbsp;    Obt_Marks  &nbsp;   Percentage <br>")
+// document.write("English &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +total+"  &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;"+english+"  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+per+"%   <br>")
+// document.write("Math &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +total+"  &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;"+math+"  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+per1+"%   <br>")
+// document.write("Urdu &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +total+"  &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;"+urdu+"  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+per2+"%   <br>")
+// document.write("Total &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  300  &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;"+obt+"  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+pertotal+"%   <br>")
+
+
+
+// Task  : 6.
 
 
 
